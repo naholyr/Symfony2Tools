f7834bce415a799bdd60bb9d38a787f35d40226d
Nicolas Chambrier <naholyr@gmail.com>
2011-03-13 00:27:10 +0100

    Fix config.yml, not working as-is (doctrine.dbal & doctrine.orm invalid options)

--- sf2_init_project.sh
+++ sf2_init_project.sh
@@ -3,15 +3,24 @@ DIR=$(pwd)
 
 echo Initialize new Symfony2 project...
 
+
+# Git init
+
 echo Check or initialize git repository...
 if [ ! -d ".git" ]; then git init; fi
+
+
+# Git config
 
 echo Check git configuration...
 if ! git config user.name > /dev/null; then echo 'No user.name defined ! Please run git config user.name "Your Name"'; exit 1; fi
 if ! git config user.email > /dev/null; then echo 'No user.email defined ! Please run git config user.email "your@email.tld"'; exit 1; fi
 
+
+# Download Symfony distribution to a temp folder and unarchive it
+
 echo Download and unzip Symfony into /tmp/Symfony...
-TMP=$(mktemp -d)
+TMP=$(mktemp -d '_sf2XXX_')
 cd $TMP
 if ! (curl http://symfony.com/get/$SF2_PKG.tgz | tar zx); then echo 'Failed downloadig Symfony2 distribution.'; exit 1; fi
 cd "$DIR"
@@ -20,6 +29,51 @@ rm -rf $TMP
 
 git add .
 git commit -a -m "Initialize new project from $SF2_PKG"
+
+
+# For PR7: config.yml is invalid for current version of doctrine
+
+echo Patching default config.yml to make it work with current Doctrine...
+echo '@@ -28,17 +28,22 @@ assetic:
+ # Doctrine Configuration
+ doctrine:
+     dbal:
+-        driver:   %database_driver%
+-        host:     %database_host%
+-        dbname:   %database_name%
+-        user:     %database_user%
+-        password: %database_password%
+-        logging:  %kernel.debug%
+-
++        default_connection: default
++        connections:
++            default:
++                driver:   %database_driver%
++                host:     %database_host%
++                dbname:   %database_name%
++                user:     %database_user%
++                password: %database_password%
++                logging:  %kernel.debug%
+     orm:
+         auto_generate_proxy_classes: %kernel.debug%
+-        mappings:
+-            AcmeDemoBundle: ~
++        default_entity_manager: default
++        entity_managers:
++            default:
++                mappings:
++                    AcmeDemoBundle: ~
+ 
+ # Swiftmailer Configuration
+ swiftmailer:
+-- 
+' | patch -p0 app/config/config.yml
+
+git add .
+git commit -a -m "Fixed config.yml"
+
+
+# Install vendors as submodules
 
 echo Install vendors...
 git submodule add git://github.com/kriswallsmith/assetic.git vendor/assetic
@@ -37,14 +91,23 @@ git submodule add git://github.com/symfony/WebConfiguratorBundle.git vendor/bund
 git add .
 git commit -a -m "Added Symfony2 standard vendors"
 
+
+# Execute assets task
+
 echo Initialize assets...
 app/console assets:install web/
+
 git add web
 git commit -a -m "Installed assets"
 
+
+# Ignore logs & cache
+
 echo '*' > app/logs/.gitignore
 echo '*' > app/cache/.gitignore
+
 git add -f app/logs/.gitignore app/cache/.gitignore
 git commit app/logs/.gitignore app/cache/.gitignore -m "Ignore logs & cache"
+
 
 echo Ready to go \!
